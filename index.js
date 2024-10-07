c6c51673f4836aef17564d201ca1b5d6ce3d0064
pranav-gujar <pranavgujar974@gmail.com>
2024-10-07 15:53:17 +0530

    All files added

---
+++ index.js
@@ -0,0 +1,40 @@
+
+//typing effect
+document.addEventListener('DOMContentLoaded', () => {
+    const textElement = document.querySelector('.animated-text');
+    let text = textElement.textContent;
+    let newText = '';
+    let index = 0;
+  
+    function typeText() {
+        if (index < text.length) {
+            newText += text[index];
+            textElement.textContent = newText;
+            index++;
+            setTimeout(typeText, 100);
+        }
+    }
+  
+    textElement.textContent = '';
+    typeText();
+  });
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const textElement = document.querySelector('.animated-text');
+    let text = textElement.textContent;
+    let newText = '';
+    let index = 0;
+  
+    function typeText() {
+        if (index < text.length) {
+            newText += text[index];
+            textElement.textContent = newText;
+            index++;
+            setTimeout(typeText, 100);
+        }
+    }
+  
+    textElement.textContent = '';
+    typeText();
+  });
+  
